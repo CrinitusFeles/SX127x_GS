78cafe12133bc6e47f5372f190c6d744c3f6a4bc
CrinitusFeles <crinitusfeles@gmail.com>
2023-11-06 16:50:56 +0700

    set serial port from sys.argv

--- sx127x_gs/__main__.py
+++ sx127x_gs/__main__.py
@@ -1,11 +1,12 @@
 
 
 import time
+import sys
 from sx127x_gs.radio_controller import RadioController
 
 
 if __name__ == '__main__':
     lora: RadioController = RadioController(interface='Serial')
-    if lora.connect(port_or_ip='COM17'):  # 192.168.0.5
+    if lora.connect(port_or_ip=sys.argv[1]):  # 192.168.0.5
         time.sleep(0.2)
         lora.user_cli()
